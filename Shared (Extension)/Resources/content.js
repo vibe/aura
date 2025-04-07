57557340791e7076957ed41d794ae9ffabdad523
vibe <francoc137@icloud.com>
2025-04-07 06:26:44 -0700

    feature(meta-theme): create observer to set custom theme color on every page
    Signed-off-by: vibe <francoc137@icloud.com>

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -5,3 +5,30 @@ browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log("Received request: ", request);
 });
+
+
+(() => {
+  const insertBlackThemeColor = () => {
+    const meta = document.createElement('meta');
+    meta.name = 'theme-color';
+    meta.content = '#000000';
+
+    if (document.head.firstChild) {
+      document.head.insertBefore(meta, document.head.firstChild);
+    } else {
+      document.head.appendChild(meta);
+    }
+  };
+
+  if (document.head) {
+    insertBlackThemeColor();
+  } else {
+    const observer = new MutationObserver((_, obs) => {
+      if (document.head) {
+        insertBlackThemeColor();
+        obs.disconnect();
+      }
+    });
+    observer.observe(document.documentElement, { childList: true, subtree: true });
+  }
+})();
